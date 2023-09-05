ec2142b16eccd5a1f17182ee3fe9f55957953473
saikrishna272295 <saikrishna272295@gmail.com>
2023-09-05 11:44:08 -0500

    Initial Commit

---
+++ src/main/java/com/sai/SpringBootJenkinsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.sai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJenkinsDockerApplication.class, args);
+	}
+
+}
